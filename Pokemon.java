45785fbe685bacf88ba7b855ee3c7340745c201c
user <user@users-MacBook-Pro.local>
2026-03-09 16:30:09 +1300

    updated pokemon, and added git ignore

--- Pokemon.java
+++ Pokemon.java
@@ -3,3 +3,7 @@ public class Pokemon {
         System.out.println("Pokemon!");
     }
 }
+
+// git remote add origin https://github.com/jtiz-gh/pokemon_281.git
+// git branch -M main
+// git push -u origin main
